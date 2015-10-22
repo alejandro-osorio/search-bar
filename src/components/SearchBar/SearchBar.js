0ce79bbdaa5e5d6e2a084779a4105c73d87dfa0f
Alejandro Osorio <alejnadro.osorio@vtex.com.br>
2015-10-22 14:25:04 -0200

    Corrigir erros no componente

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import 'SearchBar.less';
+import './SearchBar.less';
 
 class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { searchValue: null };
+    this.onSearchKeyUp = this.onSearchKeyUp.bind(this);
+    this.onMakeSearch = this.onMakeSearch.bind(this);
+    this.onSearchValueChange = this.onSearchValueChange.bind(this);
+  }
+  onSearchKeyUp(){
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+    }
+    this.timeout = setTimeout((function(){ this.onMakeSearch(); }).bind(this),1000);
+  }
+
+  onMakeSearch(){
+    this.timeout = null;
+    window.alert(this.state.searchValue);
+  }
+
+  onSearchValueChange(e){
+    this.setState({ searchValue: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <h1>My new component SearchBar!</h1>
-        <input type='search' className="searchbar" placeholder="Busque o que quiser!" />
+        <input type='search' className="searchbar" placeholder="Busque o que quiser!" value={this.state.searchValue} onKeyUp={this.onSearchKeyUp} onChange={this.onSearchValueChange}/>
       </div>
     );
   }
