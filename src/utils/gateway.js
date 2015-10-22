5f1edf8f780884b9fe5189150f1b0fedfa48e0d0
Marcelo Couto <marcelocouto@vtex.com.br>
2015-10-22 16:55:18 -0200

    Identado

--- src/utils/gateway.js
+++ src/utils/gateway.js
@@ -1,79 +1,79 @@
 import axios from 'axios';
 
 class gateway {
-    constructor() {
-        let sandbox = ('; ' + document.cookie).split('; vtex_sandbox=').pop().split(';').shift();
-        let workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
-        let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
+  constructor() {
+    let sandbox = ('; ' + document.cookie).split('; vtex_sandbox=').pop().split(';').shift();
+    let workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
+    let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
 
-        this.defaultHeaders = {
-            'Authorization': `token ${token}`,
-            'x-vtex-sandbox': sandbox,
-            'x-vtex-workspace': workspace ? workspace : 'master'
-        };
+    this.defaultHeaders = {
+      'Authorization': `token ${token}`,
+      'x-vtex-sandbox': sandbox,
+      'x-vtex-workspace': workspace ? workspace : 'master'
+    };
 
-        this.SearchResource = '/_resources/search@alphateam.search-bar/';
-    }
+    this.SearchResource = '/_resources/search@alphateam.search-bar/';
+  }
 
-    loadProducts(searchTerm, callback) {
-        return axios.get(this.SearchResource, {
-            headers: this.defaultHeaders
-        });
-    }
+  loadProducts(searchTerm, callback) {
+    return axios.get(this.SearchResource, {
+      headers: this.defaultHeaders
+    });
+  }
 }
 
 export default
 class gateway {
-    constructor() {
-        let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
-        let workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
-        let sandbox = ('; ' + document.cookie).split('; vtex_sandbox=').pop().split(';').shift();
+  constructor() {
+    let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
+    let workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
+    let sandbox = ('; ' + document.cookie).split('; vtex_sandbox=').pop().split(';').shift();
 
-        this.defaultHeaders = {
-            'Authorization': `token ${token}`,
-            'x-vtex-sandbox': sandbox,
-            'x-vtex-workspace': workspace ? workspace : 'master'
-        };
+    this.defaultHeaders = {
+      'Authorization': `token ${token}`,
+      'x-vtex-sandbox': sandbox,
+      'x-vtex-workspace': workspace ? workspace : 'master'
+    };
 
-        this.productResource = '/_resources/product@vtex.storefront-sdk/';
-        this.productsResource = '/_resources/products@vtex.storefront-sdk/';
+    this.productResource = '/_resources/product@vtex.storefront-sdk/';
+    this.productsResource = '/_resources/products@vtex.storefront-sdk/';
+  }
+
+  products(params) {
+    params = omit(params, ['$id']);
+
+    if (params.product) {
+      return axios.get(this.productResource, {
+        params,
+        headers: this.defaultHeaders
+      });
     }
 
-    products(params) {
-        params = omit(params, ['$id']);
+    return axios.get(this.productsResource, {
+      params,
+      headers: this.defaultHeaders
+    });
+  }
 
-        if (params.product) {
-            return axios.get(this.productResource, {
-                params,
-                headers: this.defaultHeaders
-            });
-        }
+  facets(params) {
+    params = omit(params, ['$id']);
 
-        return axios.get(this.productsResource, {
-            params,
-            headers: this.defaultHeaders
-        });
-    }
+    const url = this.productsResource + '/facets';
 
-    facets(params) {
-        params = omit(params, ['$id']);
+    return axios.get(url, {
+      params,
+      headers: this.defaultHeaders
+    });
+  }
 
-        const url = this.productsResource + '/facets';
+  categories(params) {
+    params = omit(params, ['$id']);
 
-        return axios.get(url, {
-            params,
-            headers: this.defaultHeaders
-        });
-    }
+    const categoryId = params.category ? params.category : '';
+    const url = this.productsResource + '/categories/' + categoryId;
 
-    categories(params) {
-        params = omit(params, ['$id']);
-
-        const categoryId = params.category ? params.category : '';
-        const url = this.productsResource + '/categories/' + categoryId;
-
-        return axios.get(url);
-    }
+    return axios.get(url);
+  }
 }
 
 export default gateway;
