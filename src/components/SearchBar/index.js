d6d77bf0ae86363cb32922a39cf587c98f98cf1b
Daniel Fosco <daniel.fosco@vtex.com.br>
2015-11-05 17:19:16 -0200

    Revert "Improve search result interaction"
    This reverts commit 4ddadb38dae604c3a716b747f60b11d710284d47.

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -53,20 +53,12 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    var results = this.state.searchResults ? this.state.searchResults : '';
-    var searchResult = _.map(results, function(result){
-      return (
-        <li className="SearchResults-item">
-         <span className="search-item-copy">{result.name}</span>
-       </li>
-      );
-    })
-
-    let mockResults = (
-      <li className="SearchResults-item">
-       <span className="search-item-copy">iPhone 6</span>
-     </li>
-    );
+    // var results = this.state.searchResults ? this.state.searchResults : '';
+    // var searchResult = _.map(results, function(result){
+    //   return (<div>
+    //     {result.name}
+    //   </div>);
+    // })
 
     let hasResults = true;
 
@@ -77,13 +69,17 @@ class SearchBar extends React.Component {
           <button className="SearchBar-close-button" onTouchTap={this.handleTouchTap}></button>
         </div>
         <ul className="SearchResults" data-has-results={hasResults}>
-          {searchResult}
-          {mockResults}
-          {mockResults}
-          {mockResults}
-          {mockResults}
-          {mockResults}
-          <li className="SearchResults-item-cta" data-has-cta={hasResults}>
+          {/*<div className="search-item">{searchResult}</div>*/}
+          <li className="SearchResults-item">
+            <span className="search-item-copy">iPhone 6</span>
+          </li>
+          <li className="SearchResults-item">
+            <span className="search-item-copy">iPhone 5s</span>
+          </li>
+          <li className="SearchResults-item">
+            <span className="SearchResults-item-copy">Samsung Galaxy S4</span>
+          </li>
+          <li className="SearchResults-item-cta">
             <a href="" className="SearchResults-item-link">Ver todos</a>
           </li>
         </ul>
